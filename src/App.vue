3470c2a6a00a30419a59c803da6453d7c7ffef30
‘98hb <w17339377595@163.com>
2022-02-11 21:06:05 +0800

    数据获取

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <MyHeader background="red" title="购物车案例"></MyHeader>
+    <MyHeader
+      background="lightyellow"
+      color="darkgreen"
+      title="购物车案例"
+    ></MyHeader>
     <div class="main"><MyGoods></MyGoods></div>
     <MyFooter></MyFooter>
   </div>
@@ -11,6 +15,7 @@
 // 1. 创建项目，下包，引入bs样式
 // 2. 拆分需求组件，创建 - 分别标签+样式
 // 3. App.vue上显示
+
 import MyHeader from "./components/MyHeader.vue";
 import MyGoods from "./components/MyGoods.vue";
 import MyFooter from "./components/MyFooter.vue";
@@ -19,6 +24,15 @@ export default {
     MyHeader,
     MyGoods,
     MyFooter,
+  },
+  created() {
+    // 不必再自己引入axios变量,而是直接使用全局属性$axios
+    // console.log(this);
+    this.$axios({
+      url: "/api/cart",
+    }).then((res) => {
+      console.log(res);
+    });
   },
 };
 </script>
