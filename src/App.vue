95f693b3d7eb8fe4659a13d8402be2b4627ed6b4
‘98hb <w17339377595@163.com>
2022-02-11 20:16:16 +0800

    项目初始化 - 静态页面

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
-  <div></div>
+  <div>
+    <MyHeader></MyHeader>
+    <div class="main"><MyGoods></MyGoods></div>
+    <MyFooter></MyFooter>
+  </div>
 </template>
 
 <script>
-export default {};
+// 目标:  项目初始化 - 静态页面
+// 1. 创建项目，下包，引入bs样式
+// 2. 拆分需求组件，创建 - 分别标签+样式
+// 3. App.vue上显示
+import MyHeader from "./components/MyHeader.vue";
+import MyGoods from "./components/MyGoods.vue";
+import MyFooter from "./components/MyFooter.vue";
+export default {
+  components: {
+    MyHeader,
+    MyGoods,
+    MyFooter,
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.main {
+  padding-top: 45px;
+  padding-bottom: 50px;
+}
 </style>
