e42a56e787a7b5cb3358172edde9a79b45c613f0
‘98hb <w17339377595@163.com>
2022-02-12 12:03:23 +0800

    商品数量控制

--- src/components/MyCount.vue
+++ src/components/MyCount.vue
@@ -1,16 +1,53 @@
 <template>
   <div class="my-counter">
-    <button type="button" class="btn btn-light">-</button>
-    <input type="number" class="form-control inp" v-model="obj.goods_count" />
-    <button type="button" class="btn btn-light">+</button>
+    <!--  <button
+      type="button"
+      class="btn btn-light"
+      :disabled="obj.goods_count === 1"
+      @click="obj.goods_count > 1 && obj.goods_count--"
+    > -->
+    <button
+      type="button"
+      class="btn btn-light"
+      :disabled="obj.goods_count === 1"
+      @click="obj.goods_count--"
+    >
+      -
+    </button>
+    <input
+      type="number"
+      class="form-control inp"
+      v-model.number="obj.goods_count"
+    />
+    <button type="button" class="btn btn-light" @click="obj.goods_count++">
+      +
+    </button>
   </div>
 </template>
 
 <script>
+//目标： 商品数量 -控制
+// 1. 外部传入数据对象
+// 2. v-model关联对象的goods_count属性和输入框(双向绑定)
+// 3. 商品按钮 =和-,商品数量最少为1
+// 4. 侦听数量改变,小于1,直接强制覆盖1
 export default {
-  props: { obj: Object },
+  props: {
+     obj: Object
+      },
   // 因为数量控制要通过对象"互相引用的关系"
   // 来影响外面对象里的数量值,所以最好传 对象进来
+  watch: {
+    obj: {
+      deep: true,
+      handler() {
+        // 拿到商品数量,判断小于1,强制修改成1
+        if (this.obj.goods_count < 1) {
+          this.obj.goods_count = 1;
+        }
+      },
+    },
+  },
 };
 </script>
 
